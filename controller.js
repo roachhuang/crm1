1c20177028f604b9e8e6cea17662b30e5747da0a
test <mark.huang@ca-sec.com>
2014-12-09 17:55:45 +0800

    add filter - query

--- controller.js
+++ controller.js
@@ -13,65 +13,8 @@
     $scope.pagedItems    =  [];
     $scope.currentPage   =  0;
 
-    $scope.phones = [
-                        {
-                            'name': 'Mexus S',
-                            'snippet': 'Fast just got faster with Nexus S.',
-                            'price':30,
-                            'quantity':50
-                        },
-                        {
-                            'name': 'Samsung Tablet™ with Wi-Fi',
-                            'snippet': 'The Next, Next Generation tablet.',
-                            'price':30,
-                            'quantity':60
-                        },
-                        {
-                            'name': 'Soney Xperia™',
-                            'snippet': 'The Next, Next Generation Phone.',
-                            'price':30,
-                            'quantity':70
-                        }
-                    ];
+    
 
-    /** Scope argument specify the function by name remove and passed index of list item as a parameter , which splice the list by 1 , as click on remove button **/
-            
-    $scope.remove = function (index) {
-        $scope.phones.splice(index,1);
-    }
-
-    $scope.funding = { startingEstimate: 0 };
-
-    $scope.computeNeeded = function() {           
-        $scope.needed = $scope.funding.startingEstimate * 10;                
-    };
-
-
-    /** Check if value for funding is 0 or more **/
-
-    $scope.requestFunding = function() {               
-        if( $scope.needed == "" || !$scope.needed )
-        window.alert("Sorry, please get more customers first.");
-    };
-
-    $scope.reset = function() {
-        $scope.funding.startingEstimate = 0;
-        $scope.needed = 0;
-    };
-
-    /** toggleMenu Function to show menu by toggle effect , by default the stage of the menu is false as we click on toggle button, we make it to true or show and reverse on anothe click event. **/
-
-    $scope.menuState = false;
-    $scope.add_prod = true;
-
-    $scope.toggleMenu = function() {                
-        if($scope.menuState) {                    
-            $scope.menuState= false;
-        }
-        else {
-            $scope.menuState= !$scope.menuState.show;
-        }
-    };
 
     /** function to get detail of product added in mysql referencing php **/
 
