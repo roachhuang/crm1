d72d363bb8f115678f2ce46ad6a702f7d5c8405f
test <mark.huang@ca-sec.com>
2014-12-10 18:10:24 +0800

    factory is added

--- controller.js
+++ controller.js
@@ -1,31 +1,30 @@
-    var listApp = angular.module('listpp', []);    
+(function(){    
+    var app = angular.module('crmApp', ['ngRoute', 'ngCsv']);    
 
-    /* variable listApp is  a variable which used to control the array values to show the data to show in view  using the module name 'listApp' with arguments as an array */
+    /* variable app is  a variable which used to control the array values to show the data to show in view  using the module name 'app' with arguments as an array */
 
     /* Initialize the controller by name 'PhoneListCtrl' holds the information of phone in form of array with keys name, snipper, price , quantity */
 
     /* $scope argument passed in function is a key arguments should be passed with exactly the same name */
 
-    listApp.controller('PhoneListCtrl', function ($scope,$http) {
+    app.controller('CrmController', ['$scope', '$http', 'dataFactory', function($scope, $http, dataFactory) {
     $scope.filteredItems =  [];
     $scope.groupedItems  =  [];
     $scope.itemsPerPage  =  3;
-    $scope.pagedItems    =  [];
+    $scope.rows =  [];
     $scope.currentPage   =  0;
-
-    
-
 
     /** function to get detail of product added in mysql referencing php **/
 
     $scope.get_product = function() {
-        $http.get("db.php?action=get_product").success(function(data)
-        {
-            //$scope.product_detail = data;   
-            $scope.pagedItems = data;    
-
-        });
-    }
+        dataFactory.getData()
+            .success(function(data){
+                $scope.rows=data;              
+            })
+            .error(function(error){
+                console.log('faile to read from db' + error.message);
+            });
+    };
 
     /** function to add details for products in mysql referecing php **/
 
@@ -45,7 +44,7 @@
         .error(function(data, status, headers, config){
            
         });
-    }
+    };
 
     /** function to delete product from list of product referencing php **/
 
@@ -63,7 +62,7 @@
         .error(function(data, status, headers, config){
            
         });
-    }
+    };
 
     /** fucntion to edit product details from list of product referencing php **/
 
@@ -88,7 +87,7 @@
         .error(function(data, status, headers, config){
            
         });
-    }
+    };
 
     /** function to update product details after edit from list of products referencing php **/
 
@@ -109,7 +108,7 @@
                 .error(function(data, status, headers, config){
                    
                 });
-    }
+    };
 
     /** function to call prvious page , click on paging items button previous for product list **/
 
@@ -117,25 +116,22 @@
         if ($scope.currentPage > 0) {
             $scope.currentPage--;
         }
-    }
+    };
 
     /** function to set current page on paging items for product list **/
 
     $scope.setPage = function() { 
         alert(this.n);
         $scope.currentPage = this.n;
-    }
+    };
 
     /** function to call next page , click on paging items button next for product list **/
-
 
     $scope.nextPage = function() {
         if ($scope.currentPage < $scope.pagedItems.length - 1) {
             $scope.currentPage++;
         }
-    }
-
-     
+    };    
 
     /** fucntion to define the total number of pages, in reference of product list items based upon itemsperpage **/
 
@@ -150,5 +146,44 @@
         }
         return ret;
     };
+
+    }]); // end of Crmcontroller
+
+    app.config(['$routeProvider', function($routeProvider){     
+        $routeProvider.
+        when("/showtable",
+            {           
+                //controller: 'tblCtrl',
+                //templateUrl: "./templates/showtable.html"
+            })      
+        .when("/import",
+            {           
+                //controller: 'addCtrl',
+                templateUrl: "./templates/import.html"
+            })  
+        .when("/export",
+            {           
+                //controller: 'addCtrl',
+                templateUrl: "./templates/export.html"
+            });  
+        // .otherwise({redirectTo: './templates/showtable'});            
+    }]);
+
+    app.factory('dataFactory', ['$http', function($http){
+        // import csv file
+        var factory = {};       
+        // "this" equal to the runtime factory
+        factory.getCsv = function(){
+            var url='./test.csv';
+            $http.get(url).then(function(response){
+                return csvParser(response.data);
+            });     
+        };
+        // read CRM table from asiayo database
+        factory.getData = function(){
+            return $http.get("./db.php?action=get_product");            
+        };  
+        return factory; // return an object
+    }]);
    
-});
+})();
