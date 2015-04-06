0f2920aa2e5e686d0145d1ac8ccc949ae3a87aae
roach <mark.huang@ca-sec.com>
2015-04-06 23:11:28 +0800

    oop for modal service

--- js/services.js
+++ js/services.js
@@ -1,58 +1,70 @@
 (function(){
+    var app = angular.module('mainApp.Services', ['ui.bootstrap']);
+    // Constrcutor
+    var BaseModal = function($modal){
+        this.ml = $modal;
+        //this.modalOptions.size = size;              
+    };    
 
-    var app = angular.module('modal', []);
-    app.service('modalService', ['$modal',
-        function ($modal) {
+    BaseModal.prototype = {
+        constructor: BaseModal,
+        modalDefaults: {
+            backdrop: true,
+            keyboard: true,
+            //modalFade: true,
+            templateUrl: './templates/modal.html'
+        },
 
-            var modalDefaults = {
-                backdrop: true,
-                keyboard: true,
-                modalFade: true,
-                templateUrl: './templates/modal.html'
-            };
+        modalOptions: {
+            closeButtonText: 'Close',
+            actionButtonText: 'OK',
+            headerText: 'Proceed?',
+            bodyText: 'Perform this action?'
+        },
 
-            var modalOptions = {
-                closeButtonText: 'Close',
-                actionButtonText: 'OK',
-                headerText: 'Proceed?',
-                bodyText: 'Perform this action?'
-            };
+        showModal:function (customModalDefaults, customModalOptions) {
+            if (!customModalDefaults) customModalDefaults = {};
+            customModalDefaults.backdrop = 'static';
+            return this.show(customModalDefaults, customModalOptions);
+        },
+        show:function (customModalDefaults, customModalOptions, $modal) {
+            //Create temp objects to work with since we're in a singleton service
+            var tempModalDefaults = {};
+            var tempModalOptions = {};
 
-            this.showModal = function (customModalDefaults, customModalOptions) {
-                if (!customModalDefaults) customModalDefaults = {};
-                customModalDefaults.backdrop = 'static';
-                return this.show(customModalDefaults, customModalOptions);
-            };
+            //Map angular-ui modal custom defaults to modal defaults defined in service
+            angular.extend(tempModalDefaults, this.modalDefaults, customModalDefaults);
+            //Map modal.html $scope custom properties to defaults defined in service
+            angular.extend(tempModalOptions, this.modalOptions, customModalOptions);
 
-            this.show = function (customModalDefaults, customModalOptions) {
-                //Create temp objects to work with since we're in a singleton service
-                var tempModalDefaults = {};
-                var tempModalOptions = {};
-
-                //Map angular-ui modal custom defaults to modal defaults defined in service
-                angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
-
-                //Map modal.html $scope custom properties to defaults defined in service
-                angular.extend(tempModalOptions, modalOptions, customModalOptions);
-
-                if (!tempModalDefaults.controller) {
-                    tempModalDefaults.controller = function ($scope, $modalInstance) {
-                        $scope.modalOptions = tempModalOptions;
-                        $scope.modalOptions.ok = function (result) {                            
-                            // this will invoke promise and return result to then()
-                            $modalInstance.close(result);   
-                        };
-                        $scope.modalOptions.close = function (result) {
-                            $modalInstance.dismiss('cancel');
-                        };
-                    }
-                }
-                        // displaying the modal dialog
-                return $modal.open(tempModalDefaults).result;
-            };
-
+            if (!tempModalDefaults.controller) {
+                tempModalDefaults.controller = function ($scope, $modalInstance) {
+                    $scope.modalOptions = tempModalOptions;
+                    $scope.modalOptions.ok = function (result) {                            
+                        // this will invoke promise and return result to then()
+                        $modalInstance.close(result);   
+                    };
+                    $scope.modalOptions.close = function (result) {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }                   
+            }
+                    // displaying the modal dialog
+                    // the result is the para of modal.clos(para)
+            return this.ml.open(tempModalDefaults).result;
         }
-    ]);
+    };
+
+    BaseModal.$inject = ['$modal'];
+    /*    
+    var MyModal = function() {}; // constructor
+    MyModal.prototype = Object.create(BaseModal.prototype);
+    MyModal.prototype.constructor = MyModal;
+
+    MyModal.prototype.backdrop=false;    
+    */
+    // here modalService = new MyModal;
+    app.service('modalService', BaseModal);
 
     app.controller('CustomerEditController', ['$scope', '$location', '$routeParams', '$timeout', 'config', 'dataService', 'modalService',
         function ($scope, $location, $routeParams, $timeout, config, dataService, modalService) {
